7321965a5dc7a56a79b2f0aa569e81760089dd81
Peachey <peacheym@dal.ca>
2022-01-26 17:28:40 -0400

    Continue adding content for the assignment. TODO: Determine required level of responsiveness.

--- Assignment1/main.js
+++ Assignment1/main.js
@@ -1,216 +1,320 @@
-      // d3.select("#header").attr('width', 500).attr('height', 500)
-      // .style("border", "1px white solid")
-      // .style("color", "white").style("font-size", "42px")
-      // .text("Coronavirus - One Year On");
+// D3 Code here
 
-      var svgContainer = d3
-        .select("body")
-        .append("svg")
-        .attr("width", window.innerWidth)
-        .attr("height", window.innerHeight * 1.5)
-        .style("border", "1px salmon solid");
+var svgContainer = d3
+  .select("body")
+  .append("svg")
+  .attr("width", window.innerWidth)
+  .attr("height", window.innerHeight * 1.5)
+  .style("border", "1px salmon solid");
 
-      var headerText = svgContainer
-        .append("text")
-        .attr("y", 90)
-        .attr("x", 30)
-        .style("font-size", "38px")
-        .style("fill", "white")
-        .text("CORONAVIRUS - ONE YEAR ON");
-      var headerSubText = svgContainer
-        .append("text")
-        .attr("y", 140)
-        .attr("x", 30)
-        .style("font-size", "48px")
-        .style("fill", "white")
-        .style("font-weight", "bold")
-        .text("TRACKING UK PUBLIC PERCEPTION");
+function generateHeader() {
+  var headerText = svgContainer
+    .append("text")
+    .attr("y", 90)
+    .attr("x", 30)
+    .style("font-size", "38px")
+    .style("fill", "white")
+    .text("CORONAVIRUS - ONE YEAR ON");
+  var headerSubText = svgContainer
+    .append("text")
+    .attr("y", 140)
+    .attr("x", 30)
+    .style("font-size", "48px")
+    .style("fill", "white")
+    .style("font-weight", "bold")
+    .text("TRACKING UK PUBLIC PERCEPTION");
 
-      var brandName = svgContainer
-        .append("text")
-        .attr("y", 90)
-        .attr("x", 1300)
-        .style("font-size", "38px")
-        .style("fill", "white")
-        .style("font-weight", "bold")
-        .text("Ipsos MORI");
+  var brandName = svgContainer
+    .append("text")
+    .attr("y", 90)
+    .attr("x", 1300)
+    .style("font-size", "38px")
+    .style("fill", "white")
+    .style("font-weight", "bold")
+    .text("Ipsos MORI");
 
-      var headerP1 = svgContainer
-        .append("text")
-        .attr("y", 190)
-        .attr("x", 30)
-        .style("font-size", "20px")
-        .style("fill", "white")
-        .text(
-          "A snapshot of Ipsos MORI's research and analysis related to the COVID-19 coronavirus pandemic, one year after the first UK lockdown was announced."
-        );
-      var headerDate = svgContainer
-        .append("text")
-        .attr("y", 220)
-        .attr("x", 30)
-        .style("font-size", "20px")
-        .style("fill", "white")
-        .style("font-weight", "bold")
-        .text("March 25th, 2021");
-      var headerP2 = svgContainer
-        .append("text")
-        .attr("y", 250)
-        .attr("x", 30)
-        .style("font-size", "20px")
-        .style("fill", "white")
-        .text("For more information, visit http://loremipsum.com ...");
+  var headerP1 = svgContainer
+    .append("text")
+    .attr("y", 190)
+    .attr("x", 30)
+    .style("font-size", "20px")
+    .style("fill", "white")
+    .text(
+      "A snapshot of Ipsos MORI's research and analysis related to the COVID-19 coronavirus pandemic, one year after the first UK lockdown was announced."
+    );
+  var headerDate = svgContainer
+    .append("text")
+    .attr("y", 220)
+    .attr("x", 30)
+    .style("font-size", "20px")
+    .style("fill", "white")
+    .style("font-weight", "bold")
+    .text("March 25th, 2021");
+  var headerP2 = svgContainer
+    .append("text")
+    .attr("y", 250)
+    .attr("x", 30)
+    .style("font-size", "20px")
+    .style("fill", "white")
+    .text("For more information, visit http://loremipsum.com ...");
+}
 
-      //  var chart1 = svgContainer.append('svg').attr('width', 540).attr('height', 250).attr("y", 300).attr("x", 270).style("border", "1px black solid")
-      //  var headerP2 = svgContainer.append("text").attr("y", 0).attr("x", 0).style("fill", "black").text("Chart 1");
+function generateChart1() {
+  /* Handle Chart Section 1 */
 
-      var chart1 = svgContainer
-        .append("svg")
-        .attr("width", 540)
-        .attr("height", 250)
-        .attr("y", 300)
-        .attr("x", 270)
-        .style("border", "1px black solid");
-      chart1.append("text").attr("y", 50).text("Chart 1");
+  var text1 = svgContainer
+    .append("svg")
+    .attr("width", 220)
+    .attr("y", 300)
+    .attr("x", 30)
+    .attr("height", 250);
+  text1
+    .append("text")
+    .attr("y", 45)
+    .attr("x", 13)
+    .style("font-weight", "bold")
+    .style("font-size", "26px")
+    .attr("fill", "white")
+    .text("PLANNING NHS");
+  text1
+    .append("text")
+    .attr("y", 70)
+    .attr("x", 13)
+    .style("font-weight", "bold")
+    .style("font-size", "26px")
+    .attr("fill", "white")
+    .text("RESPONSE");
+  text1
+    .append("text")
+    .attr("y", 110)
+    .attr("x", 13)
+    .style("font-size", "16px")
+    .attr("fill", "white")
+    .text("Confidence in the");
+  text1
+    .append("text")
+    .attr("y", 130)
+    .attr("x", 13)
+    .style("font-size", "16px")
+    .attr("fill", "white")
+    .text("NHS is at its heighest");
+  text1
+    .append("text")
+    .attr("y", 150)
+    .attr("x", 13)
+    .style("font-size", "16px")
+    .attr("fill", "white")
+    .text("Since June 2020");
 
-      var chart2 = svgContainer
-        .append("svg")
-        .attr("width", 540)
-        .attr("height", 250)
-        .attr("y", 300)
-        .attr("x", 1080)
-        .style("border", "1px black solid");
-      chart2.append("text").attr("y", 50).text("Chart 2");
+  var chart1 = svgContainer
+    .append("svg")
+    .attr("width", 540)
+    .attr("height", 250)
+    .attr("y", 300)
+    .attr("x", 270);
+  chart1.append("text").attr("y", 50).text("Chart 1");
+}
 
-      var chart3 = svgContainer
-        .append("svg")
-        .attr("width", 540)
-        .attr("height", 250)
-        .attr("y", 665)
-        .attr("x", 270)
-        .style("border", "1px black solid");
-      // chart3.append("text")
-      // .attr("y", 50)
-      // .text("Chart 3");
+function generateChart2() {
+    
+  var text2 = svgContainer
+  .append("svg")
+  .attr("width", 220)
+  .attr("y", 300)
+  .attr("x", 835)
+  .attr("height", 250);
+text2
+  .append("text")
+  .attr("y", 45)
+  .attr("x", 13)
+  .style("font-weight", "bold")
+  .style("font-size", "26px")
+  .attr("fill", "white")
+  .text("HOME TESTING");
+text2
+  .append("text")
+  .attr("y", 70)
+  .attr("x", 13)
+  .style("font-weight", "bold")
+  .style("font-size", "26px")
+  .attr("fill", "white")
+  .text("STUDY");
+text2
+  .append("text")
+  .attr("y", 110)
+  .attr("x", 13)
+  .style("font-size", "16px")
+  .attr("fill", "white")
+  .text("The largest testing");
+text2
+  .append("text")
+  .attr("y", 130)
+  .attr("x", 13)
+  .style("font-size", "16px")
+  .attr("fill", "white")
+  .text("programme for");
+text2
+  .append("text")
+  .attr("y", 150)
+  .attr("x", 13)
+  .style("font-size", "16px")
+  .attr("fill", "white")
+  .text("coronavirus shows a");
+  text2
+  .append("text")
+  .attr("y", 170)
+  .attr("x", 13)
+  .style("font-size", "16px")
+  .attr("fill", "white")
+  .text("decline in prevalence");
+text2
+  .append("text")
+  .attr("y", 190)
+  .attr("x", 13)
+  .style("font-size", "16px")
+  .attr("fill", "white")
+  .text("of the virus");
+text2
+  .append("text")
+  .attr("y", 210)
+  .attr("x", 13)
+  .style("font-size", "16px")
+  .attr("fill", "white")
+  .text("Since June 2020");
 
-      var chart4 = svgContainer
-        .append("svg")
-        .attr("width", 540)
-        .attr("height", 250)
-        .attr("y", 665)
-        .attr("x", 1080)
-        .style("border", "1px black solid");
-      chart4.append("text").attr("y", 50).text("Chart 4");
 
-      var chart5 = svgContainer
-        .append("svg")
-        .attr("width", 540)
-        .attr("height", 250)
-        .attr("y", 1020)
-        .attr("x", 270)
-        .style("border", "1px black solid");
-      chart5.append("text").attr("y", 50).text("Chart 5");
+  var chart2 = svgContainer
+    .append("svg")
+    .attr("width", 540)
+    .attr("height", 250)
+    .attr("y", 300)
+    .attr("x", 1080)
+    .style("border", "1px black solid");
+  chart2.append("text").attr("y", 50).text("Chart 2");
+}
 
-      var chart6 = svgContainer
-        .append("svg")
-        .attr("width", 540)
-        .attr("height", 250)
-        .attr("y", 1020)
-        .attr("x", 1080)
-        .style("border", "1px black solid");
-      chart6.append("text").attr("y", 50).text("Chart 6");
+function generateCharts() {
+  var chart3 = svgContainer
+    .append("svg")
+    .attr("width", 540)
+    .attr("height", 250)
+    .attr("y", 665)
+    .attr("x", 270)
+    .style("border", "1px black solid");
 
-      var radius = 120;
+  var chart4 = svgContainer
+    .append("svg")
+    .attr("width", 540)
+    .attr("height", 250)
+    .attr("y", 665)
+    .attr("x", 1080)
+    .style("border", "1px black solid");
+  chart4.append("text").attr("y", 50).text("Chart 4");
 
-      // Create dummy data
-      var data = { a: 8, b: 9, c: 18, d: 63 };
+  var chart5 = svgContainer
+    .append("svg")
+    .attr("width", 540)
+    .attr("height", 250)
+    .attr("y", 1020)
+    .attr("x", 270)
+    .style("border", "1px black solid");
+  chart5.append("text").attr("y", 50).text("Chart 5");
 
-      // set the color scale
-      var color = d3
-        .scaleOrdinal()
-        .domain(data)
-        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b"]);
+  var chart6 = svgContainer
+    .append("svg")
+    .attr("width", 540)
+    .attr("height", 250)
+    .attr("y", 1020)
+    .attr("x", 1080)
+    .style("border", "1px black solid");
+  chart6.append("text").attr("y", 50).text("Chart 6");
 
-      // Compute the position of each group on the pie:
-      var pie = d3.pie().value(function (d) {
-        return d.value;
-      });
-      var data_ready = pie(d3.entries(data));
+  var radius = 120;
 
-      // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-      chart3
-        .append("g")
-        .attr("transform", "translate(" + 220 + "," + 125 + ")")
-        .selectAll("whatever")
-        .data(data_ready)
-        .enter()
-        .append("path")
-        .attr(
-          "d",
-          d3
-            .arc()
-            .innerRadius(70) // This is the size of the donut hole
-            .outerRadius(radius)
-        )
-        .attr("fill", function (d) {
-          return color(d.data.key);
+  // Create dummy data
+  var data = { a: 8, b: 9, c: 18, d: 63 };
+
+  // set the color scale
+  var color = d3
+    .scaleOrdinal()
+    .domain(data)
+    .range(["#002454", "#2f459b", "#317372", "#214d4e"]);
+
+  // Compute the position of each group on the pie:
+  var pie = d3.pie().value(function (d) {
+    return d.value;
+  });
+  var data_ready = pie(d3.entries(data));
+
+  // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+  chart3
+    .append("g")
+    .attr("transform", "translate(" + 220 + "," + 125 + ")")
+    .selectAll("whatever")
+    .data(data_ready)
+    .enter()
+    .append("path")
+    .attr(
+      "d",
+      d3
+        .arc()
+        .innerRadius(70) // This is the size of the donut hole
+        .outerRadius(radius)
+    )
+    .attr("fill", function (d) {
+      return color(d.data.key);
+    })
+    .attr("stroke", "black")
+    .style("stroke-width", "2px")
+    .style("opacity", 0.7);
+
+  // Create the Bar Chart
+  var margin = { top: 20, right: 30, bottom: 40, left: 90 },
+    width = 540 - margin.left - margin.right,
+    height = 250 - margin.top - margin.bottom;
+
+  d3.csv("./bargraph.csv", function (data) {
+    // Add X axis
+    var x = d3.scaleLinear().domain([0, 50]).range([0, width]);
+    chart6
+      .append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x))
+      .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
+
+    // Y axis
+    var y = d3
+      .scaleBand()
+      .range([0, height])
+      .domain(
+        data.map(function (d) {
+          return d.Label;
         })
-        .attr("stroke", "black")
-        .style("stroke-width", "2px")
-        .style("opacity", 0.7);
+      )
+      .padding(0.1);
+    chart6.append("g").call(d3.axisLeft(y));
 
-      var margin = { top: 20, right: 30, bottom: 40, left: 90 },
-        width = 460 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
+    //Bars
+    chart6
+      .selectAll("myRect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("x", x(0))
+      .attr("y", function (d) {
+        return y(d.Label);
+      })
+      .attr("width", function (d) {
+        return x(d.Value);
+      })
+      .attr("height", y.bandwidth())
+      .attr("fill", "#30469c");
+  });
+}
 
-      // append the svg object to the body of the page
-      // var svg = d3.select("#my_dataviz")
-      //   .append("svg")
-      //     .attr("width", width + margin.left + margin.right)
-      //     .attr("height", height + margin.top + margin.bottom)
-      //   .append("g")
-      //     .attr("transform",
-      //           "translate(" + margin.left + "," + margin.top + ")");
-
-      // Parse the Data
-      d3.csv(
-        "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv",
-        function (data) {
-          // Add X axis
-          var x = d3.scaleLinear().domain([0, 13000]).range([0, width]);
-          chart6
-            .append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x))
-            .selectAll("text")
-            .attr("transform", "translate(-10,0)rotate(-45)")
-            .style("text-anchor", "end");
-
-          // Y axis
-          var y = d3
-            .scaleBand()
-            .range([0, height])
-            .domain(
-              data.map(function (d) {
-                return d.Country;
-              })
-            )
-            .padding(0.1);
-          chart6.append("g").call(d3.axisLeft(y));
-
-          //Bars
-          chart6
-            .selectAll("myRect")
-            .data(data)
-            .enter()
-            .append("rect")
-            .attr("x", x(0))
-            .attr("y", function (d) {
-              return y(d.Country);
-            })
-            .attr("width", function (d) {
-              return x(d.Value);
-            })
-            .attr("height", y.bandwidth())
-            .attr("fill", "#69b3a2");
-        }
-      );
+generateHeader();
+generateChart1();
+generateChart2();
+generateCharts();
