313fb3d13da1707e0888317540ea39d1378d9ef5
Peachey <peacheym@dal.ca>
2022-02-07 21:38:20 -0400

    Update some placement and text.

--- Assignment1/chart3.js
+++ Assignment1/chart3.js
@@ -56,7 +56,7 @@ function generateChart3() {
   var chart3 = svgContainer
     .append("svg")
     .attr("width", 540)
-    .attr("height", 250)
+    .attr("height", 350)
     .attr("y", 665)
     .attr("x", 270)
     .style("border", "1px black solid");
@@ -80,7 +80,7 @@ function generateChart3() {
   // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
   chart3
     .append("g")
-    .attr("transform", "translate(" + 220 + "," + 125 + ")")
+    .attr("transform", "translate(" + 220 + "," + 150 + ")")
     .selectAll("whatever")
     .data(data_ready)
     .enter()
@@ -89,7 +89,7 @@ function generateChart3() {
       "d",
       d3
         .arc()
-        .innerRadius(70) // This is the size of the donut hole
+        .innerRadius(80) // This is the size of the donut hole
         .outerRadius(radius)
     )
     .attr("fill", function (d) {
@@ -98,4 +98,47 @@ function generateChart3() {
     .attr("stroke", "black")
     .style("stroke-width", "2px")
     .style("opacity", 0.7);
+
+  // Add above pie chart text
+  chart3
+    .append("text")
+    .attr("y", 15)
+    .style("font-size", "16px")
+    .attr("fill", "#374faa")
+    .text("If a vaccine for COVID-19 were avaliable to me, I would get it");
+
+  // Add inner pie chart text
+  chart3
+    .append("text")
+    .attr("y", 115)
+    .attr("x", 220)
+    .style("font-size", "24px")
+    .style("text-anchor", "middle")
+    .style("font-weight", "bold")
+    .attr("fill", "#214d4e")
+    .text("83%");
+  chart3
+    .append("text")
+    .attr("y", 140)
+    .attr("x", 220)
+    .style("font-size", "16px")
+    .style("text-anchor", "middle")
+    .attr("fill", "#214d4e")
+    .text("in the UK agree");
+  chart3
+    .append("text")
+    .attr("y", 160)
+    .attr("x", 220)
+    .style("font-size", "16px")
+    .style("text-anchor", "middle")
+    .attr("fill", "#214d4e")
+    .text("they would get");
+  chart3
+    .append("text")
+    .attr("y", 180)
+    .attr("x", 220)
+    .style("font-size", "16px")
+    .style("text-anchor", "middle")
+    .attr("fill", "#214d4e")
+    .text("a vaccine");
 }
--- Assignment1/main.js
+++ Assignment1/main.js
@@ -59,83 +59,6 @@ function generateHeader() {
     .text("For more information, visit http://loremipsum.com ...");
 }
 
-
-function generateCharts() {
-
-
-  // var chart4 = svgContainer
-  //   .append("svg")
-  //   .attr("width", 540)
-  //   .attr("height", 250)
-  //   .attr("y", 665)
-  //   .attr("x", 1080)
-  //   .style("border", "1px black solid");
-  // chart4.append("text").attr("y", 50).text("Chart 4");
-
-  // var chart5 = svgContainer
-  //   .append("svg")
-  //   .attr("width", 540)
-  //   .attr("height", 250)
-  //   .attr("y", 1020)
-  //   .attr("x", 270)
-  //   .style("border", "1px black solid");
-  // chart5.append("text").attr("y", 50).text("Chart 5");
-
-  var chart6 = svgContainer
-    .append("svg")
-    .attr("width", 540)
-    .attr("height", 250)
-    .attr("y", 1020)
-    .attr("x", 1080)
-    .style("border", "1px black solid");
-  chart6.append("text").attr("y", 50).text("Chart 6");
-
-  // Create the Bar Chart
-  var margin = { top: 20, right: 30, bottom: 40, left: 90 },
-    width = 540 - margin.left - margin.right,
-    height = 250 - margin.top - margin.bottom;
-
-  d3.csv("./bargraph.csv", function (data) {
-    // Add X axis
-    var x = d3.scaleLinear().domain([0, 50]).range([0, width]);
-    chart6
-      .append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x))
-      .selectAll("text")
-      .attr("transform", "translate(-10,0)rotate(-45)")
-      .style("text-anchor", "end");
-
-    // Y axis
-    var y = d3
-      .scaleBand()
-      .range([0, height])
-      .domain(
-        data.map(function (d) {
-          return d.Label;
-        })
-      )
-      .padding(0.1);
-    chart6.append("g").call(d3.axisLeft(y));
-
-    //Bars
-    chart6
-      .selectAll("myRect")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("x", x(0))
-      .attr("y", function (d) {
-        return y(d.Label);
-      })
-      .attr("width", function (d) {
-        return x(d.Value);
-      })
-      .attr("height", y.bandwidth())
-      .attr("fill", "#30469c");
-  });
-}
-
 generateHeader();
 generateChart1();
 generateChart2();
@@ -143,4 +66,3 @@ generateChart3();
 generateChart4();
 generateChart5();
 generateChart6();
-generateCharts();
