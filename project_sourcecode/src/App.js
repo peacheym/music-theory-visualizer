99ef56500c380337df0c957be6110894e61e4839
Matt Peachey <peacheym@dal.ca>
2022-03-02 13:45:10 -0400

    Add all code not in htdocs.

--- project_sourcecode/src/App.js
+++ project_sourcecode/src/App.js
@@ -1,10 +1,15 @@
+import Chart from "./Chart";
 import Notes from "./Notes";
+import { withScreenSize } from "@visx/responsive";
+
+let chart = withScreenSize(<Notes />);
 
 function App() {
   return (
     <>
-      <svg height="1200px" width={"1200px"}>
-        <Notes />
+      <svg width={"100%"} height={"2000px"}>
+        <Notes/>
+        <Chart />
       </svg>
     </>
   );
--- project_sourcecode/src/Notes.js
+++ project_sourcecode/src/Notes.js
@@ -1,22 +1,56 @@
-import { Circle } from "@visx/shape";
+import { Circle, Line } from "@visx/shape";
+import { Text } from "@visx/text";
+import { curveNatural } from "@visx/curve";
 
 const root_notes = [1, 2, 3, 4, 5, 6, 7];
 
-function compute_fill(root) {
-  if (root == 1 || root == 4 || root == 5) {
-    return "cadetblue";
-  }
-  if (root == 2 || root == 3 || root == 6) {
-    return "lavender";
-  }
-  if (root == 7) {
-    return "salmon";
-  }
+function compute_fill(root, KeyTonality) {
+  if (KeyTonality === "MAJOR") {
+    switch (root) {
+      case 1:
+        return "cadetblue";
+      case 2:
+        return "lavender";
+      case 3:
+        return "lavender";
+      case 4:
+        return "cadetblue";
+      case 5:
+        return "cadetblue";
+      case 6:
+        return "lavender";
+      case 7:
+        return "salmon";
+      default:
+        return "black";
+    }
+  } else if (KeyTonality === "MINOR")
+    switch (root) {
+      case 1:
+        return "lavender";
+      case 2:
+        return "salmon";
+      case 3:
+        return "cadetblue";
+      case 4:
+        return "lavender";
+      case 5:
+        return "lavender";
+      case 6:
+        return "cadetblue";
+      case 7:
+        return "cadetblue";
+      default:
+        return "black";
+    }
 }
 
 export default function Notes() {
   return (
     <>
+      <Text x={80} y={60}>
+        Major
+      </Text>
       {root_notes.map((note) => {
         return (
           <Circle
@@ -25,7 +59,22 @@ export default function Notes() {
             cx={100 * note}
             cy={100}
             r={15}
-            fill={compute_fill(note)}
+            fill={compute_fill(note, "MAJOR")}
+          />
+        );
+      })}
+      <Text x={80} y={200}>
+        Minor
+      </Text>
+      {root_notes.map((note) => {
+        return (
+          <Circle
+            key={`note-${note}`}
+            className="dot"
+            cx={100 * note}
+            cy={240}
+            r={15}
+            fill={compute_fill(note, "MINOR")}
           />
         );
       })}
